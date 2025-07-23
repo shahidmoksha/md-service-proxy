f1cfe3e004237b9dc9a89c3e8a5d72dd744c5564
shahidmoksha <shahid@mokshadigital.com>
2025-07-23 13:15:34 +0530

    dicom-scp-service log fixes again

--- dicom-scp/dicom_scp.py
+++ dicom-scp/dicom_scp.py
@@ -137,5 +137,6 @@ monitor_thread.start()
 handlers = [(evt.EVT_C_STORE, handle_store)]
 ae = AE(ae_title=AE_TITLE)
 ae.supported_contexts = StoragePresentationContexts
-print("Starting SCP on port {%d}, AE Title: {%s}", PORT, AE_TITLE)
+message = f"Starting SCP on port {PORT}, AE Title: {AE_TITLE}"
+print(message)
 ae.start_server(("0.0.0.0", PORT), block=True, evt_handlers=handlers)
